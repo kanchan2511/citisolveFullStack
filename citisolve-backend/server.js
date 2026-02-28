2f2acdd169c178b9b2c3ea54543e6e20df0be3af
KANCHAN <kanchankumawat2509@gmail.com>
2026-02-28 22:03:43 +0530

    Comment out specific CORS configuration

--- citisolve-backend/server.js
+++ citisolve-backend/server.js
@@ -12,9 +12,10 @@ connectDB();
  
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL,
-    credentials: true
- }));
+// app.use(cors({ origin: process.env.CLIENT_URL,
+//     credentials: true
+//  }));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"))
 app.use("/uploads", express.static("uploads"));
